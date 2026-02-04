80250194c3dddd1335643ce908426a205143c7c0
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-02-04 13:07:52 +0000

    feat: add bun

--- devshell.nix
+++ devshell.nix
@@ -4,11 +4,12 @@
 }:
 pkgs.mkShellNoCC {
   packages = [
+    perSystem.self.pi-sync
+    pkgs.bun
     pkgs.datasette
     pkgs.python3
     pkgs.sqlite
     pkgs.sqlite-web
-    perSystem.self.pi-sync
   ];
   shellHook = ''
     export PRJ_ROOT=$PWD
